0d78d6434f121eda8cdf1ef3ccf332c208a5a4ca
Qadeer Assan <45184523+qadeerassan@users.noreply.github.com>
2019-01-27 11:52:07 -0500

    Add files via upload

--- cleanData.py
+++ cleanData.py
@@ -10,8 +10,6 @@ from matplotlib.pyplot import axis
 #     targ_pre = re.sub('[]')
 def setNumerics(df, colName):
     fld = df[colName]
-    
-
 
 pd.set_option('display.expand_frame_repr', False)
 
@@ -29,13 +27,13 @@ df[[c for c in df if df[c].isnull().sum() < 2]]
 # df = pd.DataFrame({"ReceivedTimestamp":pd.date_range('02:00:00', periods=4, freq='23H')})
 ftr = [3600,60,1]
 df = df[df["DeviceSerial"] != 108406724]
-print(df.head(8))
+print(df.head(5))
+timeList = []
 for i in df["ReceivedTimestamp"]:
     newStr = i
     x = sum([a*b for a,b in zip(ftr, map(float,newStr.split(':')))])
-    df["ReceivedTimestamp"] = x
-    
-print(df.head(10))
+    timeList.append(x)
+
 for i in df["TripState"]:
     print(i)
     if(i == "Engine On"):
@@ -49,8 +47,7 @@ for i in df["TripState"]:
 
 m = RandomForestRegressor(n_jobs=-1)
 
-m.fit(df["TripState"], df["ReceivedTimestamp"])
-
-
+m.fit(timeList, df["ReceivedTimestamp"])
+m.score()
 print(df.head(30))
 print("test run")
